0ce8c4dfc6b0de6f54bd6bd2ee6e1bafe0b25ef9
rageypaige <paigeyragey@gmail.com>
2020-03-21 14:20:15 -0700

    previous messages are now visible

--- alien-bot/src/main/Chatroom.java
+++ alien-bot/src/main/Chatroom.java
@@ -12,52 +12,48 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Chatroom extends Application {
-	Button button;
+	Button send, clear;
 	Stage window;
 	Scene scene;
 	TextField input;
-	Label alienMsg, humanMsg;
+	//Label alienMsg, humanMsg, alienMsg1, humanMsg1;
 	private Alien al;
 	private String alias;
 	private String humanName;
+	static int clicked;
+	final private int LOWEST_LABEL_HEIGHT=50;
+	private GridPane grid;
+	List<Label> labelList;
 
 	public static void main(String[] args) {
 		launch(args);
-		Scanner userInput = new Scanner(System.in);
-
-		/*
-		 * while(true) { // Reading input System.out.print("> "); String in =
-		 * userInput.nextLine();
-		 * 
-		 * if(in.equalsIgnoreCase("bye") || in.equalsIgnoreCase("goodbye")) // 'bye' or
-		 * 'goodbye' is exit statement break; else { //String alias = al.getNameState()
-		 * ? al.getName() : "Alien";
-		 * 
-		 * // Small wait to make it seem more realistic try { Thread.sleep(500); } catch
-		 * (InterruptedException e) { e.printStackTrace(); }
-		 * 
-		 * System.out.println(alias + ": " + al.parse(in)); } } userInput.close();
-		 */
+		//Scanner userInput = new Scanner(System.in);
+		clicked = 0;
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		//Initialize alien
+		// Initialize alien
 		al = new Alien("ET");
 		alias = al.getNameState() ? al.getName() : "Alien";
-		humanName = al.getHumanName();
+		humanName = al.getNameState() ? al.getHumanName() : "Human";
+		
+		labelList = new ArrayList();
 
 		window = primaryStage;
 		// Set title of window
 		primaryStage.setTitle("Alien Bot!");
 
-		//Set up grid
-		GridPane grid = new GridPane();
+		// Set up grid
+		grid = new GridPane();
 		// padding
-		grid.setPadding(new Insets(20, 20, 20, 20));
+		grid.setPadding(new Insets(15, 15, 15, 15));
 		// vertical spacing
 		grid.setVgap(8);
 		// Horizontal spacing
@@ -69,42 +65,83 @@ public class Chatroom extends Application {
 		 */
 		// Input field
 		TextField input = new TextField();
-		GridPane.setConstraints(input, 7, 40);
+		GridPane.setConstraints(input, 20, 55);
 
-		// Set button object and set the text
-		button = new Button();
-		button.setText("Send");
-
-		GridPane.setConstraints(button, 8, 40);
-		// Add the children to the window
-		grid.getChildren().addAll(input, button);
-
-		//Initialize Labels
-		alienMsg = new Label("A friendly alien says hello!");
-		GridPane.setConstraints(alienMsg, 7, 35);
-		grid.getChildren().add(alienMsg);
+		// Initialize buttons
+		send = new Button();
+		send.setText("Send");
+		GridPane.setConstraints(send, 21, 55);
 		
-		humanMsg = new Label();
-		GridPane.setConstraints(humanMsg, 7, 30);
-		grid.getChildren().add(humanMsg);
+		clear = new Button();
+		clear.setText("Clear");
+		GridPane.setConstraints(clear, 22, 55);
 
+		
+		// Add the children to the window
+		grid.getChildren().addAll(input, send, clear);
+
+		// Initialize Labels
+		//Label alienMsg = new Label("A friendly alien says hello!");
+		//GridPane.setConstraints(alienMsg, 20, 50);
+		//grid.getChildren().add(alienMsg);
+
+		//Label humanMsg = new Label();
+		//GridPane.setConstraints(humanMsg, 20, 45);
+		//grid.getChildren().add(humanMsg);
+		
 		// create a scene and set size of window
-		Scene scene = new Scene(grid, 700, 500);
+		Scene scene = new Scene(grid, 800, 600);
 		// Set the scene to the primary stage
 		primaryStage.setScene(scene);
 		// Display stage
 		primaryStage.show();
 
-		// method called when user hits button
-		button.setOnAction(new EventHandler<ActionEvent>() {
+		//Method called when user hits send button
+		send.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				if (event.getSource() == button) {
-					humanMsg.setText(humanName + ": " + input.getText());
-					alienMsg.setText(alias + ": " + al.parse(input.getText()));
+				//humanMsg.setText(humanName + ": " + input.getText());
+				//alienMsg.setText(alias + ": " + al.parse(input.getText()));
+				setLabels(input.getText());
+				updateGrid();
+				clicked ++;
+				if (clicked > 1) {
+					updateGrid();
+					//GridPane.setConstraints(alienMsg, 20, 35);
+					//GridPane.setConstraints(humanMsg, 20, 40);
 				}
 			}
 		});
+		
+		
+		//Method called when user hits clear button
+		clear.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				//humanMsg.setText("");
+				//alienMsg.setText("");
+			}
+		});
+	}
+	
+	public void setLabels(String in)
+	{
+		labelList.add(new Label(humanName + ": " + in));
+		labelList.add(new Label(alias + ": " + al.parse(in)));
+	}
+	
+	public void updateGrid()
+	{
+		int height = LOWEST_LABEL_HEIGHT;
+		for(int n=labelList.size()-1; n>=0; n-- ) {
+			Label l = labelList.get(n);
+			if(grid.getChildren().contains(l)) {
+				grid.getChildren().remove(l);
+			}
+			GridPane.setConstraints(l, 20, height);
+			grid.getChildren().add(l);
+			height-=5;
+		}
 	}
 
 }
