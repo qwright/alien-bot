707ab7c35b0efacf7c7ebdd1efe58ec6e138584b
rageypaige <paigeyragey@gmail.com>
2020-03-22 16:31:21 -0700

    fixed spacing issues

--- alien-bot/src/main/Chatroom.java
+++ alien-bot/src/main/Chatroom.java
@@ -11,8 +11,8 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -29,6 +29,7 @@ public class Chatroom extends Application {
 	static int clicked;
 	final private int LOWEST_LABEL_HEIGHT=50;
 	private GridPane grid;
+	RowConstraints textRow;
 	List<Label> labelList;
 
 	public static void main(String[] args) {
@@ -57,37 +58,29 @@ public class Chatroom extends Application {
 		// vertical spacing
 		grid.setVgap(8);
 		// Horizontal spacing
-		grid.setHgap(10);
+		grid.setHgap(5);
 
 		/*
 		 * Form
 		 * 
 		 */
-		// Input field
 		TextField input = new TextField();
-		GridPane.setConstraints(input, 20, 55);
+		GridPane.setConstraints(input, 20, 55, 90, 1);
+		
 
 		// Initialize buttons
 		send = new Button();
 		send.setText("Send");
-		GridPane.setConstraints(send, 21, 55);
+		GridPane.setConstraints(send, 110, 55);
 		
 		clear = new Button();
 		clear.setText("Clear");
-		GridPane.setConstraints(clear, 22, 55);
+		GridPane.setConstraints(clear, 111, 55);
 
 		
 		// Add the children to the window
 		grid.getChildren().addAll(input, send, clear);
 
-		// Initialize Labels
-		//Label alienMsg = new Label("A friendly alien says hello!");
-		//GridPane.setConstraints(alienMsg, 20, 50);
-		//grid.getChildren().add(alienMsg);
-
-		//Label humanMsg = new Label();
-		//GridPane.setConstraints(humanMsg, 20, 45);
-		//grid.getChildren().add(humanMsg);
 		
 		// create a scene and set size of window
 		Scene scene = new Scene(grid, 800, 600);
@@ -95,20 +88,18 @@ public class Chatroom extends Application {
 		primaryStage.setScene(scene);
 		// Display stage
 		primaryStage.show();
+		
+		textRow = new RowConstraints();
 
 		//Method called when user hits send button
 		send.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				//humanMsg.setText(humanName + ": " + input.getText());
-				//alienMsg.setText(alias + ": " + al.parse(input.getText()));
 				setLabels(input.getText());
 				updateGrid();
 				clicked ++;
 				if (clicked > 1) {
 					updateGrid();
-					//GridPane.setConstraints(alienMsg, 20, 35);
-					//GridPane.setConstraints(humanMsg, 20, 40);
 				}
 			}
 		});
@@ -118,8 +109,7 @@ public class Chatroom extends Application {
 		clear.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				//humanMsg.setText("");
-				//alienMsg.setText("");
+				//clear text
 			}
 		});
 	}
@@ -133,14 +123,16 @@ public class Chatroom extends Application {
 	public void updateGrid()
 	{
 		int height = LOWEST_LABEL_HEIGHT;
-		for(int n=labelList.size()-1; n>=0; n-- ) {
+		for(int n=labelList.size()-1; n>=0; n--) {
 			Label l = labelList.get(n);
+			//Remove the child
 			if(grid.getChildren().contains(l)) {
 				grid.getChildren().remove(l);
-			}
-			GridPane.setConstraints(l, 20, height);
+			} 
+			
+			GridPane.setConstraints(l, 20, height, 90, 1);
 			grid.getChildren().add(l);
-			height-=2;
+			height-=1;
 		}
 	}
 
